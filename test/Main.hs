49c92ef6620870be5e7cd38a450a04238e64587c
ascandone <alescandone@gmail.com>
2025-05-09 15:51:27 +0200

    reformat

--- test/Main.hs
+++ test/Main.hs
@@ -14,25 +14,58 @@ tests =
   Test.Tasty.testGroup
     "FundsStack.pullFrom tests"
     [ testCase "when enough balance but less than total" $
-        FundsStack.pullFrom 10 [("alice", 100)] @?= ([("alice", 10)], [("alice", 90)])
+        FundsStack.pullFrom 10 [("alice", 100)]
+          @?= ( [("alice", 10)]
+              , [("alice", 90)]
+              )
     , testCase "when enough balance but less than total (with tail)" $
-        FundsStack.pullFrom 10 [("alice", 100), ("bob", 42)] @?= ([("alice", 10)], [("alice", 90), ("bob", 42)])
+        FundsStack.pullFrom 10 [("alice", 100), ("bob", 42)]
+          @?= ( [("alice", 10)]
+              , [("alice", 90), ("bob", 42)]
+              )
     , testCase "balance is exactly the same as needed" $
-        FundsStack.pullFrom 10 [("alice", 10)] @?= ([("alice", 10)], [])
+        FundsStack.pullFrom 10 [("alice", 10)]
+          @?= ( [("alice", 10)]
+              , []
+              )
     , testCase "balance is exactly the same as needed (with tail)" $
-        FundsStack.pullFrom 10 [("alice", 10), ("bob", 42)] @?= ([("alice", 10)], [("bob", 42)])
+        FundsStack.pullFrom 10 [("alice", 10), ("bob", 42)]
+          @?= ( [("alice", 10)]
+              , [("bob", 42)]
+              )
     , testCase "balance is less than needed in the head" $
-        FundsStack.pullFrom 11 [("alice", 10), ("bob", 5)] @?= ([("alice", 10), ("bob", 1)], [("bob", 4)])
+        FundsStack.pullFrom 11 [("alice", 10), ("bob", 5)]
+          @?= ( [("alice", 10), ("bob", 1)]
+              , [("bob", 4)]
+              )
     , testCase "pull more than available" $
-        FundsStack.pullFrom 100 [("alice", 10), ("bob", 5)] @?= ([("alice", 10), ("bob", 5)], [])
+        FundsStack.pullFrom 100 [("alice", 10), ("bob", 5)]
+          @?= ( [("alice", 10), ("bob", 5)]
+              , []
+              )
     , testCase "pull remaining is zero" $
-        FundsStack.pullFrom 10 [("alice", 10), ("bob", 5)] @?= ([("alice", 10)], [("bob", 5)])
+        FundsStack.pullFrom 10 [("alice", 10), ("bob", 5)]
+          @?= ( [("alice", 10)]
+              , [("bob", 5)]
+              )
     , testCase "pull zero" $
-        FundsStack.pullFrom 0 [("alice", 10), ("bob", 5)] @?= ([], [("alice", 10), ("bob", 5)])
+        FundsStack.pullFrom 0 [("alice", 10), ("bob", 5)]
+          @?= ( []
+              , [("alice", 10), ("bob", 5)]
+              )
     , testCase "merge fragmented funds" $
-        FundsStack.pullFrom 10 [("alice", 5), ("alice", 5)] @?= ([("alice", 10)], [])
+        FundsStack.pullFrom 10 [("alice", 5), ("alice", 5)]
+          @?= ( [("alice", 10)]
+              , []
+              )
     , testCase "trim zeros" $
-        FundsStack.pullFrom 5 [("alice", 0), ("alice", 5)] @?= ([("alice", 5)], [])
+        FundsStack.pullFrom 5 [("alice", 0), ("alice", 5)]
+          @?= ( [("alice", 5)]
+              , []
+              )
     , testCase "merge fragmented store with zeros" $
-        FundsStack.pullFrom 10 [("alice", 5), ("alice", 0), ("alice", 5)] @?= ([("alice", 10)], [])
+        FundsStack.pullFrom 10 [("alice", 5), ("alice", 0), ("alice", 5)]
+          @?= ( [("alice", 10)]
+              , []
+              )
     ]
